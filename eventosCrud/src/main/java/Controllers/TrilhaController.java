f574dfdab3c454ff12cd9469ab63c65594b891dc
EdsonFreitas19 <w.edsonfreitas19@gmail.com>
2024-07-24 19:03:15 -0300

    Adicionei mas controladores para evento, seção, subevento e trilha

---
+++ eventosCrud/src/main/java/Controllers/TrilhaController.java
@@ -0,0 +1,67 @@
+package Controllers;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import Models.Trilha;
+import Persistence.PersistenceTrilha;
+
+public class TrilhaController {
+
+    private PersistenceTrilha trilhaT = new PersistenceTrilha();
+    private Scanner scanner = new Scanner(System.in);
+
+    protected void cadastrarTrilha(Trilha trilha) {
+
+        ArrayList<Trilha> trilhas = trilhaT.getTodasTrilhas();
+        boolean trilhaExistente = false;
+        for (Trilha t : trilhas) {
+            if (t.getId() == trilha.getId()) {
+                trilhaExistente = true;
+                break;
+            }
+        }
+        if (trilhaExistente) {
+            System.out.println("Trilha com ID " + trilha.getId() + " já existe.");
+        } else {
+            trilhaT.addTrilha(trilha);
+            System.out.println("Trilha cadastrada com sucesso.");
+        }
+    }
+
+    protected void listarTrilhas() {
+        ArrayList<Trilha> trilhas = trilhaT.getTodasTrilhas();
+        if (trilhas.isEmpty()) {
+            System.out.println("Nenhuma trilha cadastrada.");
+        } else {
+            for (Trilha trilha : trilhas) {
+                System.out.println("Nome da trilha: " + trilha.getNome());
+                System.out.println("ID da trilha: " + trilha.getId());
+                System.out.println("ID da seção: " + trilha.getIdSecao());
+                System.out.println("----------------------------");
+            }
+        }
+    }
+    
+    protected void deletarTrilha() {
+        System.out.println("Digite o ID da trilha que deseja excluir:");
+        int id = scanner.nextInt();
+        ArrayList<Trilha> trilhas = trilhaT.getTodosSubEventos();
+        boolean trilhaExistente = false;
+        for (Trilha t : trilhas) {
+            if (t.getId() == id) {
+                trilhaExistente = true;
+                trilhaT.deleteTrilha(t);
+                System.out.println("Trilha com ID " + id + " excluída com sucesso.");
+                break;
+            }
+        }
+        if (!trilhaExistente) {
+            System.out.println("Trilha com ID " + id + " não encontrada.");
+        }
+    }
+}
+// 1. Adicionar trilha Já implementado
+    // 2. Listar trilha  Já implementado
+    // 3. Atualizar trilha 
+    // 4. Deletar trilha  Já implementado
+    // 5. Voltar
