91b71f5c5db95527512941d27f5f14efd67d98e2
Lucas <lucasleaopessoal@gmail.com>
2024-10-23 22:09:11 -0300

    Adicionado COntroler de Gerenciamento de Eventos Adicionado Controler de Listagem de Eventos

---
+++ src/main/java/screenscontrollers/MenuADM_GerEventController.java
@@ -0,0 +1,93 @@
+package screenscontrollers;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class MenuADM_GerEventController {
+
+    @FXML
+    private Button botaoVoltar;
+
+    @FXML
+    private Button botaoAdicionarEvento;
+
+    @FXML
+    private Button botaoListarEventos;
+
+    @FXML
+    private Button botaoAtualizarEvento;
+
+    @FXML
+    private Button botaoDeletarEvento;
+
+    @FXML
+    void voltarParaMenuADM() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/screens/MenuADM.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) botaoVoltar.getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void irParaTelaAdicionarEvento() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/screens/MenuADM_ger_event_AddEvent.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) botaoAdicionarEvento.getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void irParaTelaListarEventos() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/screens/MenuADM_ger_event_listarevent.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) botaoListarEventos.getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void irParaTelaAtualizarEvento() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/screens/MenuADM_ger_event_AtualizarEvento_tela1.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) botaoAtualizarEvento.getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void irParaTelaDeletarEvento() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/screens/MenuADM_ger_event_deletar.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) botaoDeletarEvento.getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
