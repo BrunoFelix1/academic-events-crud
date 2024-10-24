c79333b23b6e3dc854aea472dff777ced967698b
EdsonFreitas19 <w.edsonfreitas19@gmail.com>
2024-10-24 13:50:23 -0300

    Removendo mais comentários desnecessários e declarando as classes dos controladores do menu palestrante

---
+++ src/main/java/screenscontrollers/MenuPalestranteAtualizarAtividadeConfController.java
@@ -0,0 +1,10 @@
+package screenscontrollers;
+
+import interfaces.IControladorTelas;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class MenuPalestranteAtualizarAtividadeConfController implements IControladorTelas {
+
+}
