3de1da3282ae363812dd17ef07345ca7ea4eb931
ytalo <ytalo4869@gmail.com>
2024-11-16 14:53:25 -0300

    Classe de conexão finalizada

--- src/main/java/database/ConexaoSQLServer.java
+++ src/main/java/database/ConexaoSQLServer.java
@@ -5,20 +5,21 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexaoSQLServer {
-        String url = "jdbc:sqlserver://192.168.0.124:1433;databaseName=academic;encrypt=true;trustServerCertificate=true";
+        private static String url = "jdbc:sqlserver://192.168.0.124:1433;databaseName=academic;encrypt=true;trustServerCertificate=true";
         //private String url = "jdbc:sqlserver://localhost:1433;databaseName=academic;encrypt=false";
-        private String usuario = "sa";
-        private String senha = "1234";
-        public void  Conectar(){
+        private static String usuario = "sa";
+        private static String senha = "1234";
+
+        public static Connection  Conectar(){
         try {
             Connection conexao = DriverManager.getConnection(url, usuario, senha);
             System.out.println("Conexão estabelecida com sucesso!");
+            return conexao;
 
-            // Fechar a conexão ao final
-            conexao.close();
         } catch (SQLException e) {
             System.out.println("Erro ao conectar: " + e.getMessage());
             e.printStackTrace();
+            return null;
         }
         }
     }
